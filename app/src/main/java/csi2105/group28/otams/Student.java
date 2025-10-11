92c871de5e357ad2244352429918de64684271f0
ch4im44 <celha054@uottawa.ca>
2025-10-11 19:16:31 -0400

    remove courses offered

--- app/src/main/java/csi2105/group28/otams/Student.java
+++ app/src/main/java/csi2105/group28/otams/Student.java
@@ -1,11 +1,8 @@
 package csi2105.group28.otams;
 
-import java.util.ArrayList;
-
 public class Student extends User {
 
     private String programOfStudy;
-    private ArrayList<String> coursesOffered;
 
     /*
      * Constructor for the Student class.
@@ -15,7 +12,6 @@ public class Student extends User {
      * @param password is the password of the student
      * @param phoneNum is the phone number of the student
      * @param programOfStudy is the program of study for the student
-     * @param coursesOffered is a list of courses offered by the student
      */
 
     /**
@@ -32,29 +28,5 @@ public class Student extends User {
      */
     public void setProgramOfStudy(String programOfStudy) {
         this.programOfStudy = programOfStudy;
-    }
-
-    /**
-     * Getter for the courses offered.
-     * @return the list of courses offered by the student
-     */
-    public ArrayList<String> getCoursesOffered() {
-        return coursesOffered;
-    }
-
-    /**
-     * Setter for the courses offered.
-     * @param coursesOffered is the new list of courses offered by the student
-     */
-    public void setCoursesOffered(ArrayList<String> coursesOffered) {
-        this.coursesOffered = coursesOffered;
-    }
-
-    /**
-     * Method to add a course to the list of courses offered.
-     * @param course is the course to be added to the list of courses
-     */
-    public void addCourse(String course) {
-        this.coursesOffered.add(course);
     }
 }
